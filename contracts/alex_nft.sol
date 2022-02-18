684a048d8b294d5f3f15150270da79fb6c9adb7b
ALEX TAM <alexufo88@hotmail.com>
2022-02-18 15:27:15 +0800

    Add files via upload

---
+++ contracts/alex_nft.sol
@@ -0,0 +1,6 @@
+// ERC72 smart contract
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
